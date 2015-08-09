a20e8a69f36f93d81b4a4de4c4c2cd9960ba9d45
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2015-08-09 23:19:07 +0200

    base functions added

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
 (function() {
     var pomodoroApp = angular.module("pomodoroApp", []);
 
+    function updateResetButton(disabled) {
+        var resetButton = $(".js-clock-reset-button");
 
+        resetButton.attr("disabled", disabled);
+    }
+
+    pomodoroApp.controller("ClockCtrl", ["$scope", "$interval", function($scope, $interval) {
+        var intervalPromise = null;
+        $scope.isTicking = false;
+        $scope.timeLeft = defaultSessionTime;
+        $scope.onOffButtonText = "Start";
+
+        $scope.clockOnOff = function() {
+            $scope.isTicking = !$scope.isTicking;
+
+            if(!$scope.isTicking && intervalPromise !== null)
+            {
+                $interval.cancel(intervalPromise);
+                $scope.onOffButtonText = "Start";
+            }
+            else if($scope.isTicking)
+            {
+                intervalPromise = $interval(function() {
+                    $scope.timeLeft -= 1000;
+                }, 1000);
+                $scope.onOffButtonText = "Pause";
+            }
+
+            updateResetButton($scope.isTicking);
+        };
+
+        $scope.clockReset = function() {
+            if($scope.isTicking)
+                return;
+
+            $scope.timeLeft = defaultSessionTime;
+        };
+    }]);
 })();
---
+++ config.js
@@ -0,0 +1,2 @@
+var defaultSessionTime = 25*60*1000;
+var defaultBreakTime = 5*60*1000;
